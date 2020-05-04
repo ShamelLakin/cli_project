8b0b08416fe3f67ac9b4e0fb47f21902849e87a2
shamellakin <christlife4u@gmail.com>
2020-05-04 10:48:27 -0400

    Getting response

--- lib/cli_hero/api.rb
+++ lib/cli_hero/api.rb
@@ -1,11 +1,14 @@
 class Api 
-    def self.get_heroes(person)
+    def self.get_heroes(super_person)
         ## what endpoint will I need
+        url = "https://superheroapi.com/api/853701521804821/1"
         ## how do I go there and get what I need
+        response = Net::HTTP.get(URI(url))
+        binding.pry
         ## how do I handle that json and turn it into data
         ## how do I make superheroes objects with that data
     end 
 
 end 
 
-## service file/class. It's responsible for talking to my api - grabbing info and returning it.
+## service file/class. It's responsible for talking to my api - grabbing info and returning it.s
