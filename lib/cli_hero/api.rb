a34a895c373b145f150e3896853f689bf48c71c6
shamellakin <christlife4u@gmail.com>
2020-05-05 10:05:07 -0400

    parsing

--- lib/cli_hero/api.rb
+++ lib/cli_hero/api.rb
@@ -4,11 +4,15 @@ class Api
         url = "https://www.superheroapi.com/api.php/853701521804821/search/#{super_person}"
         ## how do I go there and get what I need?
         response = Net::HTTP.get(URI(url))
-        binding.pry
         ## how do I handle that json and turn it into data
+        hero_results = JSON.parse(response)["results"]
         ## how do I make superheroes objects with that data
+        hero_results.each do |hero_objs|
+            SuperHero.new(hero_objs["name"], hero_objs["powerstats"], hero_objs["biography"], hero_objs["id"])
+        end
+        binding.pry 
     end 
 
 end 
 
-## service file/class. It's responsible for talking to my api - grabbing info and returning it.s
+## service file/class. It's responsible for talking to my api - grabbing info and returning it.
