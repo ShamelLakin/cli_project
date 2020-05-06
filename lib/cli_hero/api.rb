447ed8fd805d2346d692f339c3f87b3c900d90d7
shamellakin <christlife4u@gmail.com>
2020-05-06 17:16:43 -0400

    new url news

--- lib/cli_hero/api.rb
+++ lib/cli_hero/api.rb
@@ -1,16 +1,17 @@
 class Api 
     def self.get_heroes(super_person)
         ## what endpoint will I need?
-        url = "https://www.superheroapi.com/api.php/853701521804821/search/id"
+        url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=692c440b97e44049af1629e9efe064f4"
         ## how do I go there and get what I need?
         response = Net::HTTP.get(URI(url))
+        binding.pry
         ## how do I handle that json and turn it into data
-        hero_results = JSON.parse(response)["results"]
+        hero_results = JSON.parse(response)["articles"]
         ## how do I make superheroes objects with that data
         hero_results.each do |hero_objs|
             SuperHero.new(hero_objs["name"], hero_objs["powerstats"], hero_objs["biography"], hero_objs["id"])
         end
-        binding.pry 
+
     end 
 
 end 
